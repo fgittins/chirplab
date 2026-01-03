b23840bc2d093b729744350f26a1802328b9facb
Fabian Gittins <fabiangittins@gmail.com>
2026-01-03 17:11:07 +0100

    Add type hints for `Results` object

--- typings/dynesty/utils.pyi
+++ typings/dynesty/utils.pyi
@@ -1,5 +1,8 @@
 from typing import Any, NamedTuple, TypedDict
 
+import numpy
+from dynesty import bounding
+
 class ProposalStatsDict(TypedDict, total=False):
     n_proposals: int
     n_accept: int
@@ -27,4 +30,25 @@ class IteratorResult(NamedTuple):
     proposal_stats: None | ProposalStatsDict
 
 class Results:
+    logl: numpy.typing.NDArray[numpy.floating]
+    samples_it: numpy.typing.NDArray[numpy.int_]
+    samples_id: numpy.typing.NDArray[numpy.int_]
+    samples_n: numpy.typing.NDArray[numpy.int_]
+    samples_u: numpy.typing.NDArray[numpy.floating]
+    samples: numpy.typing.NDArray[numpy.floating]
+    niter: int
+    ncall: numpy.typing.NDArray[numpy.int_]
+    logz: numpy.typing.NDArray[numpy.floating]
+    logzerr: numpy.typing.NDArray[numpy.floating]
+    logwt: numpy.typing.NDArray[numpy.floating]
+    eff: float
+    nlive: int
+    logvol: numpy.typing.NDArray[numpy.floating]
+    information: numpy.typing.NDArray[numpy.floating]
+    bound: list[bounding.Bound]
+    bound_iter: numpy.typing.NDArray[numpy.int_]
+    samples_bound: numpy.typing.NDArray[numpy.int_]
+    scale: numpy.typing.NDArray[numpy.floating]
+    blob: numpy.typing.NDArray[Any]
+    proposal_stats: numpy.typing.NDArray[Any]
     def summary(self) -> None: ...
